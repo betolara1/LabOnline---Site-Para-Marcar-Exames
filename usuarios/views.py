b81d238c276b41257a8a6b58ca06ecb08d125612
roberto <robertokantovitzlara@hotmail.com>
2023-10-12 07:24:49 -0300

    primeiro commit

---
+++ usuarios/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    else:
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não são iguais.')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            # PESQUISAR COMO VALIDAR
+            # USERNAME CADASTRADOS
+
+            user = User.objects.create_user(
+                first_name=primeiro_nome,
+                last_name=ultimo_nome,
+                username=username,
+                email=email,
+                password=senha,
+            )
+        except:
+            return redirect('/usuarios/cadastro')
+    return redirect('/usuarios/cadastro')
+
