deeb24520489571001cb96c53f3558ac7d6e35e5
roberto <robertokantovitzlara@hotmail.com>
2023-10-14 07:09:39 -0300

    começo da aula 2 - pagina exames feita, falta colocar o for na pagina html

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import TiposExames
 
-# Create your views here.
+@login_required
+def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all()
+    if request.method == "GET":
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
+    
